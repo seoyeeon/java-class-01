1cc43f795d7b075831f5e1ef09bd51b3a49a3376
seoyeeon <105402849+seoyeeon@users.noreply.github.com>
2022-06-22 15:15:35 +0900

    Create Practice.java

---
+++ Practice.java
@@ -0,0 +1,15 @@
+package folder;
+
+public class Practice {
+
+	public static void main(String[] args) {// main + ctrl + space >> public static void main(String[] args){}
+		
+		System.out.println("문자열 입니다.");
+		System.out.println("문자열안에 내용은 그대로 출력이 됩니다.");
+		
+		System.out.println("1 + 5");
+		System.out.println(1 + 5);
+		// ()안에 값을 계산 후 6을 출력
+	}
+
+}
